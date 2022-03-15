78d66aa06fa08fa1725151445685d0f8a229bdec
CodeTheDev <code@codeslab.ca>
2022-03-15 01:42:58 -0400

    Repaired OAuth scripts
    - Repaired OAuth scripts after upstream merge.

--- func/oauth-callback.js
+++ func/oauth-callback.js
@@ -41,7 +41,7 @@ export async function handler(event, context) {
                     grant_type: "authorization_code",
                     code: event.queryStringParameters.code,
                     redirect_uri: new URL(event.path, DEPLOY_PRIME_URL),
-                    scope: "identify"
+                    scope: "identify email"
                 })
             });
     
@@ -78,7 +78,8 @@ export async function handler(event, context) {
                 id: user.id,
                 avatar: user.avatar,
                 username: user.username,
-                discriminator: user.discriminator
+                discriminator: user.discriminator,
+                email: user.email
             };
     
             return {
